c4a34bbf5be17f9e69f40f6f3713b135729c4cb0
Philrobots <philippe.vincent.402@gmail.com>
2026-02-05 12:07:48 -0500

    Upgrade Vulnerabilities

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ if (!WINSTONAI_API_KEY) {
 
 const server = new McpServer({
   name: "Winston AI MCP Server",
-  version: "1.0.5",
+  version: "1.0.8",
   websiteUrl: "https://github.com/gowinston-ai/winston-ai-mcp-server",
 });
 
